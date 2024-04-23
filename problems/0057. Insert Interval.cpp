5ed386e2a3a7f9e1fea949e4b5442f5b654d35c5
ryan-shen <zx1230516@gmail.com>
2024-04-24 02:24:38 +0800

    0057

--- problems/0057. Insert Interval.cpp
+++ problems/0057. Insert Interval.cpp
@@ -1,4 +1,5 @@
 #include "../shared/shared.h"
+#include <algorithm>
 #include <iostream>
 #include <math.h>
 #include <vector>
@@ -6,34 +7,30 @@ using namespace std;
 
 /*
   時間複雜度：O(n)
+  空間複雜度：O(n)
 */
 class Solution {
 public:
   vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
     vector<vector<int>> res;
+    int i = 0;
 
-    int start = -1;
-    int end = -1;
-    bool status = 0;
-    for (int i = 0; i < intervals.size(); i++) {
-      // 結束插入了
-      if (status == 2) {
-        res.push_back(intervals[i]);
-        continue;
-      }
+    // 複製與新區間左側不重疊的部分
+    for (; i < intervals.size() && intervals[i][1] < newInterval[0]; i++) {
+      res.push_back(intervals[i]);
+    }
 
-      if (intervals[i][0] <= newInterval[0] && intervals[i][1] >= newInterval[0]) {
-        start = intervals[i][0];
-        status = 1;
-        continue;
-      }
+    // 找出重疊部分的最小與最大值，找到後推入此值當合併後的結果
+    int overlapMin = newInterval[0];
+    int overlapMax = newInterval[1];
+    for (; i < intervals.size() && intervals[i][0] <= newInterval[1]; i++) {
+      overlapMin = min(overlapMin, intervals[i][0]);
+      overlapMax = max(overlapMax, intervals[i][1]);
+    }
+    res.push_back({overlapMin, overlapMax});
 
-      if (intervals[i][0] <= newInterval[1] && intervals[i][1] >= newInterval[1]) {
-        end = intervals[i][1];
-        status = 2;
-        continue;
-      }
-
+    // 複製與新區間右側不重疊的部分
+    for (; i < intervals.size() && intervals[i][0] > newInterval[1]; i++) {
       res.push_back(intervals[i]);
     }
 
@@ -43,10 +40,13 @@ public:
 
 int main(int argc, const char* argv[]) {
   Solution* solution = new Solution();
-  int input = 1;
+  vector<vector<int>> intervals = {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
+  vector<int> newInterval = {4, 8};
 
-  int result = solution->f(input);
-  cout << result << endl;
+  vector<vector<int>> result = solution->insert(intervals, newInterval);
+  for (auto vec : result) {
+    cout << "[" << vec[0] << "," << vec[1] << "] ";
+  }
 
   return 0;
 }
